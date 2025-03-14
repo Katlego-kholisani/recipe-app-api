7f40f224d787eca4816c7694f1f66e36263c4402
kholisani <katlegokholisani@gmail.com>
2025-03-14 20:15:36 +0200

    fixing flake 8 errors

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,13 +1,26 @@
 """
 Django command to wait for the database to be available.
 """
+import time
+
 from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+import psycopg2
 
 
 class Command(BaseCommand):
     """Django command to wait for database."""
 
     def handle(self, *args, **options):
-        pass
+        """Handle the command to wait for the database."""
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while not db_up:
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (psycopg2.OperationalError, OperationalError):
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
 
-    
+        self.stdout.write(self.style.SUCCESS("Database available!"))
