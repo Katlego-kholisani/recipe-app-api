51785071f4cab5794954c1200ce01cd85f2ecff6
kholisani <katlegokholisani@gmail.com>
2025-03-15 16:24:04 +0200

    configuration of databse, create user model and setup of Django admin

--- app/core/models.py
+++ app/core/models.py
@@ -8,6 +8,7 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     """Manager for users."""
 
@@ -41,4 +42,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-
