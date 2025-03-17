097a2d32130cb268a08e9e232b0bc003457ff7e3
kholisani <katlegokholisani@gmail.com>
2025-03-17 08:19:58 +0200

    Write test for recipe model and Implement recipe model

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,8 +22,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
+
 def home(request):
     return HttpResponse("Welcome to the API!")
+
 
 urlpatterns = [
     path('', home, name='home'),  # Add this line for the root URL
