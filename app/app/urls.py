0740d476b55bb9d9f1ecd2c685e7579c224877e1
kholisani <katlegokholisani@gmail.com>
2025-03-19 22:18:53 +0200

    Configure project for static files

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,6 +21,8 @@ from drf_spectacular.views import (
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 def home(request):
@@ -39,3 +41,9 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )
